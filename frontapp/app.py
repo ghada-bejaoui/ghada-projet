66e002bae92bbba9f0ec6a11f2e3e43bf1f4e032
ghada-bejaoui <67515744+ghada-bejaoui@users.noreply.github.com>
2021-12-16 07:05:14 +0100

    push9

--- frontapp/app.py
+++ frontapp/app.py
@@ -1,20 +1,12 @@
 from flask import Flask
 from flask import render_template
-import os
-
-PEOPLE_FOLDER = os.path.join('static', 'people_photo')
 
 app=Flask(__name__, template_folder='templates')
-app.config['UPLOAD_FOLDER'] = PEOPLE_FOLDE
-
-
 
 @app.route('/upload')
 def upload_files():
-   full_filename = os.path.join(app.config['UPLOAD_FOLDER'], '/static/bg.png')
-   return render_template("form.html", user_image = full_filename)
-
-
-		
+    return render_template('form.html')
+    		
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=9090) 
+
